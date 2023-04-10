fa2a2ae51e510e9cfa41bf8c56547c24b5a9bbd4
Meraz Ali <meraz@insystlabs.com>
2023-04-10 23:13:19 +0530

    simple FPS

--- testfps.py
+++ testfps.py
@@ -9,7 +9,7 @@ if __name__ == '__main__' :
 
  
     if int(major_ver)  < 3 :
-        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)
+        fps = video.get(cv2.cv.CV_CAP_PROP_FPS)  
         print ("Frames per second using video.get(cv2.cv.CV_CAP_PROP_FPS): {0}".format(fps))
     else :
         fps = video.get(cv2.CAP_PROP_FPS)
